c8a7c97618d71b4f3af137f885f8131fd318b68d
SPN288 <spnirala288@gmail.com>
2024-10-15 10:16:41 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const mongoose = require('mongoose');
+
+
+
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.mongo_url);
+    console.log("Connected to MongoDB");
+
+    const db = mongoose.connection.db;
+    const collection = db.collection("food_items"); // Replace with your collection name
+
+    const data = await collection.find({}).toArray();
+    console.log(data);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+module.exports = mongoDB;
