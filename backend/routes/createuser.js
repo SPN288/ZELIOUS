fb9bc600df20bce00aa8cd6540008abdb4a3bfb0
SPN288 <spnirala288@gmail.com>
2024-10-16 08:26:39 +0530

    update

--- backend/routes/createuser.js
+++ backend/routes/createuser.js
@@ -21,8 +21,29 @@ router.post("/createuser", [
             password: req.body.password,
             email: req.body.email,
             location: req.body.location
-        }).then(res.json({sucess:true}))
+        }).then(res.json({ sucess: true }))
         res.json({ sucess: true });
+    } catch (error) {
+        console.log(error);
+        res.json({ sucess: false });
+    }
+})
+router.post("/loginuser", [
+    // username must be an email
+    check('email').isEmail(),
+    // password must be at least 5 chars long
+    check('password', "incorrect password").isLength({ min: 5 })
+], async (req, res) => {
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    let email = req.body.email;
+    try {
+        let userdata = await User.findOne({ email });
+        if (!userdata) { return res.status(400).json({ error: "try with correct email" }) }
+        if (req.body.password !== userdata.password) { return res.status(400).json({ error: "try with correct email" }) }
+        return res.json({ sucess: true });
+
     } catch (error) {
         console.log(error);
         res.json({ sucess: false });
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
+
+
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+    const[credentials,setcredentials]=useState({email:"",password:""})
+
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        const response= await fetch("http://localhost:5000/api/createuser",{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                                 email:credentials.email,
+                                 password:credentials.password,
+            })
+        })
+        const json= await response.json();
+        console.log(json);
+        if(!json.success){
+            alert("enter valid credentials");
+        }
+    }
+    const onChange=(event)=>{
+        setcredentials({...credentials,[event.target.name]:event.target.value})
+    }
+
+    return (
+        <><div className='container'>
+
+            <form onSubmit={handleSubmit}>
+                
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={credentials.email} onChange={onChange} />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange} />
+                </div>
+                
+
+                <button  type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/createuser" className='m-3 btn btn-danger'>I am a user</Link>
+            </form>
+        </div>
+        </>
+    )
 }
