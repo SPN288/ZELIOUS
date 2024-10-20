a9dac293db42e5aad4a6082e56a03251548039bc
SPN288 <spnirala288@gmail.com>
2024-10-20 09:58:56 +0530

    update

--- forntend/src/screens/Home.js
+++ forntend/src/screens/Home.js
@@ -11,7 +11,8 @@ export default function Home() {
     const [foodItems, setfooditems] = useState([]);
     const loadData = async () => {
         //let response = await fetch("http://localhost:5000/api/foodData"
-        let response = await fetch("https://zelious.onrender.com/api/foodData"  , {
+        let response = await fetch("https://zelious.onrender.com/api/foodData"
+              , {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
